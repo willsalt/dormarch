f8b9c1e4bfc0d3d77a2882fccd38323219754ba5
Will Salt <forestpines@gmail.com>
2020-06-24 19:52:33 +0100

    Boilerplate Blazor app

---
+++ src/Dormarch/Dormarch.OpenType/FileHandling/IDataAccessor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dormarch.OpenType.FileHandling
+{
+    public interface IDataAccessor : IDisposable
+    {
+    }
+}
---
+++ src/Dormarch/Dormarch/Program.cs
@@ -0,0 +1,27 @@
+using Blazor.FileReader;
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Dormarch
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddFileReaderService(opts => opts.UseWasmSharedBuffer = true);
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
